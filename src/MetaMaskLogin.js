22aa3626fc31a80ce3a3d93c2782c81be258c8d6
K.Thirumal Reddy <76861614+klreddy2636@users.noreply.github.com>
2023-12-22 10:48:28 +0530

    Update MetaMaskLogin.js

--- src/MetaMaskLogin.js
+++ src/MetaMaskLogin.js
@@ -5,7 +5,8 @@ import { useWeb3 } from './useWeb3';
 const MetaMaskLogin = () => {
   const { web3, accounts, contract } = useWeb3();
   const [address, setAddress] = useState('');
-
+  console.log(web3);
+  console.log(accounts)
   const loginWithMetaMask = async () => {
     try {
       const accounts = await window.ethereum.request({
@@ -28,12 +29,50 @@ const MetaMaskLogin = () => {
   };
 
   return (
-    <div>
-      <h2>Login with MetaMask</h2>
-      <button onClick={loginWithMetaMask}>Login with MetaMask</button>
-      {address && <p>Welcome back! Address: {address}</p>}
+    <div style={styles.container}>
+      <h2 style={styles.title}>Login with MetaMask</h2>
+      <button style={styles.button} onClick={loginWithMetaMask}>
+        Login with MetaMask
+      </button>
+      {address && (
+        <div style={styles.addressContainer}>
+          <p style={styles.welcomeText}>Welcome back!</p>
+          <p style={styles.address}>Address: {address}</p>
+        </div>
+      )}
     </div>
   );
+};
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    padding: '20px',
+  },
+  title: {
+    fontSize: '28px',
+    marginBottom: '20px',
+    color: '#333',
+  },
+  button: {
+    padding: '10px 15px',
+    fontSize: '18px',
+    backgroundColor: '#4CAF50',
+    color: 'white',
+    borderRadius: '4px',
+    cursor: 'pointer',
+  },
+  addressContainer: {
+    marginTop: '20px',
+  },
+  welcomeText: {
+    fontSize: '20px',
+    color: '#333',
+  },
+  address: {
+    fontSize: '16px',
+    color: '#555',
+  },
 };
 
 export default MetaMaskLogin; 
